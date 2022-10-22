3cf84c087cfc4cd51bc74d335587f6ecf4cd9f79
Gyeong a Kim <alwayz0121@gmail.com>
2022-10-22 16:44:23 +0900

    finish vanila | counter

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Vanilla Redux
 
 Learning Vanilla-Redux and React-Redux
+
+### Redux의 원리
+
+store : data 저장소
+
+action : reducer()와 소통하는 방법. type을 갖고 있는 객체여야 한다.
+reducer() : data를 수정하는 함수. reducer가 return 하는 모든 것은 app의 state가 된다.(data)
+
+dispatch : dispatch를 이용해 reducer에게 메시지(action)를 보낸다. dispatch가 리듀서를 불러서 현재 state와 action을 보낸다.
+
+subscribe : store의 변화를 알 수 있다. HTML을 업데이트 한다
--- src/index.js
+++ src/index.js
@@ -4,20 +4,41 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 //reducer : data를 수정하는 함수 : return 하는 모든 것은 data로 됨
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
 //store : data 저장소
 const countStore = createStore(countModifier);
 
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+//subscribe : store의 변화를 알 수 있다.
+countStore.subscribe(onChange);
+
 //dispatch로 reducer에게 메시지(action)를 보내는 것
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
